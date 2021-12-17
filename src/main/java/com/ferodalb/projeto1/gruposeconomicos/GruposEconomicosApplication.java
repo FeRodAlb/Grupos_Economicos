052ba3ce175030d9867a18701eb5ad7b826d1efb
Felipe <felipe.r.albuquerque@gmail.com>
2021-12-17 11:38:02 -0300

    Projeto inicial

---
+++ src/main/java/com/ferodalb/projeto1/gruposeconomicos/GruposEconomicosApplication.java
@@ -0,0 +1,13 @@
+package com.ferodalb.projeto1.gruposeconomicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruposEconomicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruposEconomicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferodalb/projeto1/gruposeconomicos/GruposEconomicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferodalb.projeto1.gruposeconomicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruposEconomicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
